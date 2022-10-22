21efb976ee6a7389388c52091d6a013a2199e9db
Ritik Wandale <82282307+Ritiksw@users.noreply.github.com>
2022-10-22 18:05:41 +0530

    calculator complted

---
+++ script.js
@@ -0,0 +1,19 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+// This function display values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+// This function evaluates the expression and returns result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+
+
+// The JavaScript eval() function evaluates an expression that you pass to it. It returns the result of that expression.
